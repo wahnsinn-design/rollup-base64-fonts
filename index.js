ee95fab60caca88932efe3bd9b9ebf130b378ea7
Chris Heinz <chrisi.heinz@web.de>
2023-03-13 09:56:14 +0100

    fork and modify rollup plugin

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const { createFilter } = require("@rollup/pluginutils");
-const {readFileSync} = require("fs");
+const { createFilter } = require('@rollup/pluginutils');
+const { readFileSync } = require('fs');
 
 function base64(opts = {}) {
   if (!opts.include) {
-    throw Error("include option must be specified");
+    throw Error('include option must be specified');
   }
 
   const filter = createFilter(opts.include, opts.exclude);
+
+  const prefix = 'data:application/x-font-woff2;charset=utf-8;base64,';
   return {
-    name: "base64",
+    name: 'base64',
     transform(data, id) {
       if (filter(id)) {
         const fileData = readFileSync(id);
-          return  `export default "${fileData.toString('base64')}";`
+        return `export default "${prefix + fileData.toString('base64')}";`;
       }
-    }
+    },
   };
 }
 
